acabb6b51a82f4314af809bf35a369cdaac4b25c
dimanranawaka <dimanranawaka@gmail.com>
2023-09-06 03:13:58 +0530

    UI - ManageUserForm.fxml , ManageUserFormController.java

---
+++ src/lk/ijse/D24_H_M_S/controller/ManageUserFormController.java
@@ -0,0 +1,38 @@
+package lk.ijse.D24_H_M_S.controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+
+public class ManageUserFormController {
+    public JFXTextField txtId;
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public JFXTextField showPW;
+    public TableColumn clmId;
+    public TableColumn clmName;
+    public TableColumn clmEmail;
+    public TableColumn clmRole;
+    public JFXTextField showCMPW;
+    public JFXComboBox cmbRole;
+
+    public void slipToName(ActionEvent actionEvent) {
+    }
+
+    public void slipToEmail(ActionEvent actionEvent) {
+    }
+
+    public void slipToPassword(ActionEvent actionEvent) {
+    }
+
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+
+    }
+}
