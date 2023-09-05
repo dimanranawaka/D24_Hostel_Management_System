12e506fe4874e7ae7334299806c1c5f6a4df0644
dimanranawaka <dimanranawaka@gmail.com>
2023-09-06 00:22:49 +0530

    UI - ManageStudentForm.fxml , ManageStudentFormController.java

---
+++ src/lk/ijse/D24_H_M_S/controller/ManageStudentFormController.java
@@ -0,0 +1,48 @@
+package lk.ijse.D24_H_M_S.controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+
+public class ManageStudentFormController {
+    public JFXTextField txtId;
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtContact;
+    public TableColumn clmId;
+    public TableColumn clmName;
+    public TableColumn clmAddress;
+    public TableColumn clmContact;
+    public TableColumn clmDOB;
+    public TableColumn clmGender;
+    public JFXTextField txtDOB;
+    public JFXComboBox cmbGender;
+
+    public void slipToName(ActionEvent actionEvent) {
+    }
+
+    public void slipToAddress(ActionEvent actionEvent) {
+    }
+
+    public void slipToContact(ActionEvent actionEvent) {
+    }
+
+    public void SlipToDOB(ActionEvent actionEvent) {
+    }
+
+    public void unPaidStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void searchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void slipToGender(ActionEvent actionEvent) {
+    }
+}
