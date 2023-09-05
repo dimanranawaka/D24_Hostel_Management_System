77a4e840edc07b5e4efd5485119221f66d4e40e6
dimanranawaka <dimanranawaka@gmail.com>
2023-09-06 00:32:19 +0530

    UI - ReservationForm.fxml , ReservationFormController.java

---
+++ src/lk/ijse/D24_H_M_S/controller/ReservationFormController.java
@@ -0,0 +1,19 @@
+package lk.ijse.D24_H_M_S.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class ReservationFormController {
+    public TableView tblRes;
+    public TableColumn clmStdId;
+    public TableColumn clmName;
+    public TableColumn clmRoomId;
+    public TableColumn clmRType;
+    public TableColumn clmResId;
+    public TableColumn clmStatus;
+    public TableColumn clmDate;
+
+    public void loadEmployeeReportOnAction(ActionEvent actionEvent) {
+    }
+}
