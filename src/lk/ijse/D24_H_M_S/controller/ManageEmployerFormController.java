d4af054d465fd3c474dcfc5caf54b6331abcce64
dimanranawaka <dimanranawaka@gmail.com>
2023-09-06 00:00:55 +0530

    UI - ManageEmployerForm.fxml , ManageEmployerFormController.java

---
+++ src/lk/ijse/D24_H_M_S/controller/ManageEmployerFormController.java
@@ -0,0 +1,42 @@
+package lk.ijse.D24_H_M_S.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+
+public class ManageEmployerFormController {
+    public JFXTextField txtId;
+    public JFXTextField txtName;
+    public JFXTextField txtAddress;
+    public JFXTextField txtContact;
+    public TableColumn clmId;
+    public TableColumn clmName;
+    public TableColumn clmAddress;
+    public TableColumn clmContact;
+    public TableColumn clmRole;
+    public JFXTextField txtRole;
+
+    public void slipToName(ActionEvent actionEvent) {
+    }
+
+    public void slipToAddress(ActionEvent actionEvent) {
+    }
+
+    public void slipToContact(ActionEvent actionEvent) {
+    }
+
+    public void SlipToRole(ActionEvent actionEvent) {
+    }
+
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void searchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+}
