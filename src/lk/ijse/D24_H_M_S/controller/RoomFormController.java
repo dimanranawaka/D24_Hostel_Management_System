1a7b1dae288056c7d082558a19fae45761f4afd8
dimanranawaka <dimanranawaka@gmail.com>
2023-09-05 22:22:04 +0530

    UI - RoomForm.fxml , RoomFormController.java

---
+++ src/lk/ijse/D24_H_M_S/controller/RoomFormController.java
@@ -0,0 +1,37 @@
+package lk.ijse.D24_H_M_S.controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+
+public class RoomFormController {
+    public JFXTextField txtId;
+    public JFXTextField txtName;
+    public JFXTextField txtMoney;
+    public JFXTextField txtQty;
+    public TableColumn clmId;
+    public TableColumn clmType;
+    public TableColumn clmMoney;
+    public TableColumn clmQty;
+
+    public void slipToType(ActionEvent actionEvent) {
+    }
+
+    public void slipToMoney(ActionEvent actionEvent) {
+    }
+
+    public void slipToQty(ActionEvent actionEvent) {
+    }
+
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void searchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+}
