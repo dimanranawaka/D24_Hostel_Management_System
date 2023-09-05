e7ee51375034626471d0034bc4c2b004284cb3c5
dimanranawaka <dimanranawaka@gmail.com>
2023-09-05 21:13:59 +0530

    UI - DashBoardForm.fxml , DashBoardFormController.java

---
+++ src/lk/ijse/D24_H_M_S/controller/DashBoardFormController.java
@@ -0,0 +1,43 @@
+package lk.ijse.D24_H_M_S.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class DashBoardFormController {
+    public AnchorPane pane;
+    public AnchorPane dashBoardPane;
+    public JFXButton txtId1;
+    public JFXButton txtId2;
+    public JFXButton txtId3;
+    public JFXButton txtId4;
+    public JFXButton txtStudents;
+    public JFXButton txtEmployees;
+    public Text txtTime;
+    public Text txtDate;
+
+    public void loadLoginFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadDashBoardUiOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadRegisterOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadRoomOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadEmployeeOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadReportOnAction(ActionEvent actionEvent) {
+    }
+
+    public void openSettingPageOnAction(ActionEvent actionEvent) {
+    }
+}
