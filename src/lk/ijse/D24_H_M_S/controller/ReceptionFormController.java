2fe1a0f9339a7d3ceee6c69410cf2e95f86a389e
dimanranawaka <dimanranawaka@gmail.com>
2023-09-06 00:50:36 +0530

    UI - ReceptionForm.fxml , ReceptionFormController.java

---
+++ src/lk/ijse/D24_H_M_S/controller/ReceptionFormController.java
@@ -0,0 +1,34 @@
+package lk.ijse.D24_H_M_S.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class ReceptionFormController {
+    public AnchorPane pane;
+    public AnchorPane dashBordPane;
+    public JFXButton txtId1;
+    public JFXButton txtId2;
+    public JFXButton txtId3;
+    public JFXButton txtId4;
+    public JFXButton txtStudent;
+    public JFXButton txtEmployee;
+    public Text txtDate;
+    public Text txtTime;
+
+    public void loadDashbordUiOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadRegisterOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadStudentOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadRoomOnAction(ActionEvent actionEvent) {
+    }
+
+    public void loadLoginFormOnAction(ActionEvent actionEvent) {
+    }
+}
