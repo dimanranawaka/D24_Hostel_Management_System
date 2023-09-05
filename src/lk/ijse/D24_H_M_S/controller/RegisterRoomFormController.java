5e7cd08feb46a8806a3ade9f1fec4c8156b72985
dimanranawaka <dimanranawaka@gmail.com>
2023-09-05 21:53:02 +0530

    UI - RegisterRoomForm.fxml , RegisterRoomFormController.java

---
+++ src/lk/ijse/D24_H_M_S/controller/RegisterRoomFormController.java
@@ -0,0 +1,49 @@
+package lk.ijse.D24_H_M_S.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class RegisterRoomFormController {
+    public JFXTextField txtSID;
+    public JFXTextField txtAddress;
+    public JFXTextField txtSName;
+    public JFXTextField txtContact;
+    public JFXDatePicker txtDOB;
+    public JFXComboBox cmbGender;
+    public JFXComboBox cmbStatus;
+    public JFXComboBox cmbRid;
+    public TableView tblRoom;
+    public JFXButton btnRegister;
+    public TableColumn clmType;
+    public TableColumn clmKMoney;
+    public TableColumn clmQty;
+
+    public void slipToName(ActionEvent actionEvent) {
+    }
+
+    public void slipToContact(ActionEvent actionEvent) {
+    }
+
+    public void slipToAddress(ActionEvent actionEvent) {
+    }
+
+    public void slipToDOB(ActionEvent actionEvent) {
+    }
+
+    public void slipToGender(ActionEvent actionEvent) {
+    }
+
+    public void slipToRid(ActionEvent actionEvent) {
+    }
+
+    public void loadRoomOnAction(ActionEvent actionEvent) {
+    }
+
+    public void registerOnAction(ActionEvent actionEvent) {
+    }
+}
