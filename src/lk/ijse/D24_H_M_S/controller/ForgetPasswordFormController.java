4aed70660f4e87a86ba5e0f513c00100a4c6aaef
dimanranawaka <dimanranawaka@gmail.com>
2023-09-05 23:38:49 +0530

    UI - ForgetPasswordForm.fxml , ForgetPasswordFormController.java

---
+++ src/lk/ijse/D24_H_M_S/controller/ForgetPasswordFormController.java
@@ -0,0 +1,27 @@
+package lk.ijse.D24_H_M_S.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+public class ForgetPasswordFormController {
+    public JFXTextField txtId;
+    public JFXTextField txtName;
+    public JFXTextField showPW;
+    public JFXTextField showConfirmPW;
+    public JFXButton btnConfirmForget;
+    public JFXCheckBox cbxShow;
+
+    public void loadLoginFormOnAction(ActionEvent actionEvent) {
+    }
+
+    public void slipToName(ActionEvent actionEvent) {
+    }
+
+    public void slipToPassword(ActionEvent actionEvent) {
+    }
+
+    public void showOnAction(ActionEvent actionEvent) {
+    }
+}
