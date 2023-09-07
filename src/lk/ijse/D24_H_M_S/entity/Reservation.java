f4bdc26f860a49d41fcb1afcb80476092aac0129
dimanranawaka <dimanranawaka@gmail.com>
2023-09-07 18:57:24 +0530

    Implement - Room.java , BugFix - Reservation.java

--- src/lk/ijse/D24_H_M_S/entity/Reservation.java
+++ src/lk/ijse/D24_H_M_S/entity/Reservation.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 @Entity
 @NoArgsConstructor
@@ -16,7 +15,12 @@ public class Reservation {
     private String resId;
     private LocalDate date;
     private String status;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "student_id",referencedColumnName = "student_id")
     private Student student;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "room_type_id",referencedColumnName = "room_type_id")
     private Room room;
 
 }
--- src/lk/ijse/D24_H_M_S/entity/Room.java
+++ src/lk/ijse/D24_H_M_S/entity/Room.java
@@ -1,4 +1,34 @@
 package lk.ijse.D24_H_M_S.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Room {
+    @Id
+    @Column(name = "room_type_id")
+    private String rId;
+    private String type;
+    @Column(name = "key_money")
+    private String keMoney;
+
+    private int qty;
+
+    @OneToMany(mappedBy = "room" , fetch = FetchType.EAGER)
+    private List <Room> roomDetails = new ArrayList<>();
+
+    public Room(String rId, String type, String keMoney, int qty) {
+        this.rId = rId;
+        this.type = type;
+        this.keMoney = keMoney;
+        this.qty = qty;
+    }
 }
