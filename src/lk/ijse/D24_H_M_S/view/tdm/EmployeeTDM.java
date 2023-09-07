317b045ebdd490c1b43fed802977ac21cdca2e0f
dimanranawaka <dimanranawaka@gmail.com>
2023-09-07 19:08:39 +0530

    Implement - EmployeeTDM.java

---
+++ src/lk/ijse/D24_H_M_S/view/tdm/EmployeeTDM.java
@@ -0,0 +1,16 @@
+package lk.ijse.D24_H_M_S.view.tdm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class EmployeeTDM {
+    String eId;
+    String name;
+    String address;
+    String contact;
+    String role;
+}
