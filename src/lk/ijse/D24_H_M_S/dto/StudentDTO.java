9e89af38b7ee9aa9105d19b3a6984c1b670cd897
dimanranawaka <dimanranawaka@gmail.com>
2023-09-07 19:35:23 +0530

    Implement - StudentDTO.java

---
+++ src/lk/ijse/D24_H_M_S/dto/StudentDTO.java
@@ -0,0 +1,31 @@
+package lk.ijse.D24_H_M_S.dto;
+
+import lk.ijse.D24_H_M_S.entity.Reservation;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class StudentDTO {
+    private String sId;
+    private String name;
+    private String address;
+    private String contactNo;
+    private LocalDate date;
+    private String gender;
+    private List<Reservation> studentDetails = new ArrayList<>();
+
+    public StudentDTO(String sId, String name, String address, String contactNo, LocalDate date, String gender) {
+        this.sId = sId;
+        this.name = name;
+        this.address = address;
+        this.contactNo = contactNo;
+        this.date = date;
+        this.gender = gender;
+    }
+}
