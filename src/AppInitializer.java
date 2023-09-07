5da5f9a889191d8e88c279c82506b35335baabfc
dimanranawaka <dimanranawaka@gmail.com>
2023-09-07 16:35:58 +0530

    Implement - AppInitializer.java

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,5 +1,9 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AppInitializer extends Application {
 
@@ -8,7 +12,10 @@ public class AppInitializer extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("lk/ijse/D24_H_M_S/view/LoginForm.fxml"))));
+        primaryStage.setTitle("Login Form");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
     }
 }
