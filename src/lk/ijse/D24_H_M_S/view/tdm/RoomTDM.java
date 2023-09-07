ef7f46f417a8c7574889d18c7d8cd9cbe4ff8df9
dimanranawaka <dimanranawaka@gmail.com>
2023-09-07 19:11:07 +0530

    Implement - RoomTDM.java

---
+++ src/lk/ijse/D24_H_M_S/view/tdm/RoomTDM.java
@@ -0,0 +1,15 @@
+package lk.ijse.D24_H_M_S.view.tdm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class RoomTDM {
+    private String rId;
+    private String type;
+    private String keyMoney;
+    private int qty;
+}
