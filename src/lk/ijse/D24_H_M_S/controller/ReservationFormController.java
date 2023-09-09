b16c2fc35c4fdfa541345c5d1941547be01c2619
dimanranawaka <dimanranawaka@gmail.com>
2023-09-09 13:15:41 +0530

    Content Updated

--- src/lk/ijse/D24_H_M_S/controller/ReservationFormController.java
+++ src/lk/ijse/D24_H_M_S/controller/ReservationFormController.java
@@ -1,8 +1,13 @@
 package lk.ijse.D24_H_M_S.controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import lk.ijse.D24_H_M_S.bo.BOFactory;
+import lk.ijse.D24_H_M_S.bo.custom.ReservationBO;
+import lk.ijse.D24_H_M_S.view.tdm.CustomTDM;
 
 public class ReservationFormController {
     public TableView tblRes;
@@ -14,6 +19,40 @@ public class ReservationFormController {
     public TableColumn clmStatus;
     public TableColumn clmDate;
 
+    private final ReservationBO reservationBO = (ReservationBO) BOFactory.getBoFactory().getBO(BOFactory.Types.RESERVATION);
+
+    public void initialize(){
+
+        loadAll();
+        setCellValues();
+
+    }
+
     public void loadEmployeeReportOnAction(ActionEvent actionEvent) {
+
+    }
+    private void loadAll(){
+        try {
+
+            ObservableList<CustomTDM> allData = reservationBO.getAllData();
+            tblRes.setItems(allData);
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+        }
+    }
+
+    private void setCellValues(){
+
+        clmStdId.setCellValueFactory(new PropertyValueFactory<>("sId"));
+        clmDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+        clmName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        clmRoomId.setCellValueFactory(new PropertyValueFactory<>("rId"));
+        clmRType.setCellValueFactory(new PropertyValueFactory<>("type"));
+        clmStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+        clmResId.setCellValueFactory(new PropertyValueFactory<>("resId;"));
+
     }
 }
